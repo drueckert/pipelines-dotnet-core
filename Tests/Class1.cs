f0f49f47cc06f837316867a0a54d38073442aba9
David Rueckert <david.rueckert@churchofjesuschrist.org>
2020-06-09 12:00:50 -0600

    added test

---
+++ Tests/Class1.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+        //Given
+            Assert.Equal(4, Add(2,2));
+        //When
+        
+        //Then
+        } 
+
+        [Fact]
+        public void FailingTest()
+        {
+        //Given
+            Assert.Equal(5, Add(2,2));
+        //When
+        
+        //Then
+        }
+
+        int Add(int x, int y)
+        {
+            return x+y;
+        }
+    }
+}
